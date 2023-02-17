6d2de390dc9141217a0bd0dc89b463b052fed79a
Fantomasurfin <62075402+Fantomasurfin@users.noreply.github.com>
2023-02-17 22:06:24 +0300

    Create Game.py

---
+++ TrojanCommands/Game.py
@@ -0,0 +1,14 @@
+import vk
+
+from Keyboard import Keyboard
+from TrojanCommands import message
+from Parser import *
+from DT import *
+from Keyboard import *
+
+
+# В разработке
+class Game:
+
+    # все функции скрыты на данный момент
+    pass
